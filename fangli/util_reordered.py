7240b2eae2665f01fc3579abede1e89c0331899a
Fan Xu <fanxu@fb.com>
2018-03-10 16:10:42 -0500

    boosting

---
+++ fangli/util_reordered.py
@@ -0,0 +1,23 @@
+# these are the fifteen malware classes we're looking for
+malware_classes = ["None", "Swizzor", "VB", "Agent", "AutoRun", "FraudLoad", "FraudPack", "Hupigon", "Krap",
+           "Lipler", "Magania", "Poison", "Tdss", "Virut", "Zbot"]
+origin_malware_classes = ["Agent", "AutoRun", "FraudLoad", "FraudPack", "Hupigon", "Krap",
+           "Lipler", "Magania", "None", "Poison", "Swizzor", "Tdss",
+           "VB", "Virut", "Zbot"]
+major_classes = ["None", "Swizzor", "VB", "Others"]
+minor_classes = ["Agent", "AutoRun", "FraudLoad", "FraudPack", "Hupigon", "Krap",
+           "Lipler", "Magania", "Poison", "Tdss", "Virut", "Zbot"]
+
+# a function for writing predictions in the required format
+def write_predictions(predictions, ids, outfile):
+    """
+    assumes len(predictions) == len(ids), and that predictions[i] is the
+    index of the predicted class with the malware_classes list above for 
+    the executable corresponding to ids[i].
+    outfile will be overwritten
+    """
+    with open(outfile,"w+") as f:
+        # write header
+        f.write("Id,Prediction\n")
+        for i, history_id in enumerate(ids):
+            f.write("%s,%d\n" % (history_id, predictions[i]))
